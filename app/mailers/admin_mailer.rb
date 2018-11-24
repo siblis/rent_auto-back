96d354174ba03ac3aa9ed7e3917e5c32595f2e0a
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-11-24 13:36:30 +0300

    Реализована отправка копии заявки на почту клиенту (если он оставил адрес)

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,7 @@
 # Дефолтный мейлер для отправки писем администратору проката
 class AdminMailer < ApplicationMailer
   def request_email
-    @parameters = params[:parameters]
-    mail(to: ENV['ADMIN_EMAIL'], subject: I18n.t('mailer.subjects.request'))
+    @p = params[:parameters]
+    mail(to: ENV['ADMIN_EMAIL'], subject: I18n.t('mailer.subjects.request.new'))
   end
 end
