121360413505ffdb33869ab1dd0bdc9eea29063d
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-11-24 12:45:35 +0300

    Использовал hashie::Mash для доступа к параметрам запроса.

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,6 +3,8 @@ class RequestsController < ApplicationController
   # POST /requests
   # POST /requests.json
   def create
-    AdminMailer.with(parameters: params).request_email.deliver_now
+    # rp - request parameters
+    rp = Hashie::Mash.new(params)
+    AdminMailer.with(parameters: rp).request_email.deliver_now
   end
 end
